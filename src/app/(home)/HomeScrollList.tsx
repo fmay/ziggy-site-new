620a37e57f1299ef8230091bc43cca2bbb09499a
Freddy May <freddymay@mac.com>
2025-11-27 07:17:06 +0000

    tweaks

--- src/app/(home)/HomeScrollList.tsx
+++ src/app/(home)/HomeScrollList.tsx
@@ -8,9 +8,7 @@ const HomeScrollList: FC = () => {
     {
       content: (
         <div>
-          <h2 className="text-4xl font-bold text-gray-900 mb-6">
-            Single Instance → Ziggy Cluster
-          </h2>
+          <h2 className="text-4xl font-bold text-gray-900 mb-6">Single Instance → Ziggy Cluster</h2>
           <p className="text-lg text-gray-600 mb-4">
             Most integrations run happily on a single $12 per month cloud server. Most migrations
             will run on your laptop.
@@ -28,26 +26,21 @@ const HomeScrollList: FC = () => {
       content: (
         <div>
           <h2 className="text-4xl font-bold text-gray-900 mb-6">Integrations and migrations</h2>
-          <p>We pride ourselves on making integrations and migrations as easy as they can possibly be. Ziggy Blocks make this possible. Each block does one thing and one thing only.</p>
-          <CTA className="mt-20" label="About Ziggy Blocks" href="flows" />
-          <p className="text-lg text-gray-600 mb-4">
-            Whether you are moving millions of records from one platform to another or updating one
-            platform when data changes in another, Ziggy is fast to learn yet incredibly powerful
-            and packed with everything you need to
-          </p>
-          <ul>
-            <li>
-              Connect to any platform <strong>easily</strong>
-            </li>
-            <li>Monitor</li>
-            <li>Alert</li>
-            <li>Stay within rate limits</li>
-            <li>Work in Production and Development modes</li>
-            <li>Manage secrets and connections</li>
-            <li>Schedule flow execution</li>
-            <li>and much, much more</li>
-          </ul>
-          <CTA className="mt-20" label="Feature overview" href="flows" />
+          <div>
+            <p>
+              We pride ourselves on making integrations and migrations as easy as they can possibly
+              be. Ziggy Blocks make this possible. Each block does one thing and one thing only.
+            </p>
+          </div>
+          <CTA className="mt-10 mb-20" label="About Ziggy Blocks" href="flows" />
+          <div className="mt-40">
+            <p className="text-lg text-gray-600 mb-4">
+              Whether you are moving millions of records from one platform to another or updating
+              one platform when data changes in another, Ziggy is fast to learn yet incredibly
+              powerful.
+            </p>
+          </div>
+          <CTA className="mt-10" label="Feature overview" href="flows" />
         </div>
       ),
       image: '/images/flows/js-debugger.png',
