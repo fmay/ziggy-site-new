3b0a2f3271d94aa496edf8b62620c9e75bd994e5
Freddy May <freddymay@mac.com>
2025-11-27 11:25:49 +0000

    canvas 0.14

--- src/app/(home)/Test.tsx
+++ src/app/(home)/Test.tsx
@@ -6,18 +6,18 @@ import Konva from 'konva'
 import ImageFlip, { ImageFlipHandle } from '@/components/canvas/ImageFlip'
 import LineDraw, { LineDrawHandle } from '@/components/canvas/LineDraw'
 import Friendly from '@/app/(home)/canvas/Friendly'
+import Performance from '@/app/(home)/canvas/Performance'
 
 interface TestProps {}
 
 const Test: FC<TestProps> = ({}) => {
-  const redShapeRef = useRef<Konva.Line>(null)
-  const imageFlipRef = useRef<ImageFlipHandle>(null)
   const lineDrawRef = useRef<LineDrawHandle>(null)
-  // Initial rectangle: bottom-left, bottom-right, top-right, top-left
-  const [points, setPoints] = useState([50, 150, 150, 150, 150, 50, 50, 50])
   const [flipTrigger, setFlipTrigger] = useState(0)
+  const [flipTriggerPerformance, setFlipTriggerPerformance] = useState(0)
   const [restoreTrigger, setRestoreTrigger] = useState(0)
-  const [friendlyY, setFriendlyY] = useState(50)
+  const [restoreTriggerPerformance, setRestoreTriggerPerformance] = useState(0)
+  const [friendlyY, setFriendlyY] = useState(200)
+  const [performanceY, setPerformanceY] = useState(50)
 
   const handleCanvasClick = () => {
     // Trigger Friendly flip
@@ -31,7 +31,8 @@ const Test: FC<TestProps> = ({}) => {
   useEffect(() => {
     if (flipTrigger > 0) {
       const timer = setTimeout(() => {
-        setFriendlyY(150)
+        setFriendlyY(50)
+        setPerformanceY(200)
         setRestoreTrigger(prev => prev + 1)
       }, 3000)
 
@@ -43,21 +44,22 @@ const Test: FC<TestProps> = ({}) => {
     <div>
       <Stage width={600} height={600} onClick={handleCanvasClick}>
         <Layer>
+          <Performance flip={flipTriggerPerformance} restore={restoreTriggerPerformance} initialX={200} initialY={performanceY} />
           <Friendly flip={flipTrigger} restore={restoreTrigger} initialX={200} initialY={friendlyY} />
-          <LineDraw
-            ref={lineDrawRef}
-            x={400}
-            y={20}
-            path={[
-              { x: 0, y: 0 },
-              { x: 0, y: 100 },
-              { x: 100, y: 100 },
-            ]}
-            stroke={2}
-            color="black"
-            duration={2000}
-            deleteDelay={1000}
-          />
+          {/*<LineDraw*/}
+          {/*  ref={lineDrawRef}*/}
+          {/*  x={400}*/}
+          {/*  y={20}*/}
+          {/*  path={[*/}
+          {/*    { x: 0, y: 0 },*/}
+          {/*    { x: 0, y: 100 },*/}
+          {/*    { x: 100, y: 100 },*/}
+          {/*  ]}*/}
+          {/*  stroke={2}*/}
+          {/*  color="black"*/}
+          {/*  duration={2000}*/}
+          {/*  deleteDelay={1000}*/}
+          {/*/>*/}
         </Layer>
       </Stage>
     </div>
