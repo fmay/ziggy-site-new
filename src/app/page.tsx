5c0a38d9d449d8510a5cdd362b20e20f26b4306c
Freddy May <freddymay@mac.com>
2025-11-27 13:59:00 +0000

    canvas 0.22 refactored image flip

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
     <div className="min-h-screen max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
       <Hero />
 
-      <Test />
+      {/*<Test />*/}
 
       <HomeScrollList />
 
