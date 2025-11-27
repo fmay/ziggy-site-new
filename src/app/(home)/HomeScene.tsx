c4fe70230435c89b9103ab484edd886d6963379e
Freddy May <freddymay@mac.com>
2025-11-27 18:32:22 +0000

    editing home scene 0.2

--- src/app/(home)/HomeScene.tsx
+++ src/app/(home)/HomeScene.tsx
@@ -7,17 +7,24 @@ import LineDraw, { LineDrawHandle } from '@/components/canvas/LineDraw'
 import ImageMorph, { ImageMorphHandle } from '@/components/canvas/ImageMorph'
 
 const HomeScene: FC = () => {
-  const lineDrawRef = useRef<LineDrawHandle>(null)
   const CardFast = useRef<ImageFlipHandle>(null)
   const CardOneInstance = useRef<ImageFlipHandle>(null)
   const CardCluster = useRef<ImageFlipHandle>(null)
-  const CRM = useRef<ImageMorphHandle>(null)
-  const ERP = useRef<ImageMorphHandle>(null)
-  const LineCRM = useRef<LineDrawHandle>(null)
+
+  const MorphCRMLeft = useRef<ImageMorphHandle>(null)
+  const MorphERPLeft = useRef<ImageMorphHandle>(null)
+
+  const LineDBLeft = useRef<LineDrawHandle>(null)
+  const LineCRMLeft = useRef<LineDrawHandle>(null)
+  const LineERPLeft = useRef<LineDrawHandle>(null)
+
+  const DiffY = 60
+  const NumCards = 3
 
   // Scene definition
   const sceneDefinition: SceneDefinition = {
     steps: [
+      // STEP 1
       {
         delay: 0, // Start immediately
         duration: 1500, // This step takes 1500ms to complete
@@ -26,29 +33,21 @@ const HomeScene: FC = () => {
             target: CardCluster,
             actions: [
               { type: 'flip', direction: 'front', duration: 1500 },
-              { type: 'moveRelative', x: 0, y: 200, duration: 1500 },
+              { type: 'moveRelative', x: 0, y: NumCards * (DiffY+20), duration: 1500 },
               { type: 'fade', opacity: 0, duration: 1500 },
             ],
           },
           {
-            target: LineCRM,
+            target: LineCRMLeft,
             actions: [{ type: 'draw' }],
           },
           {
             target: CardOneInstance,
-            actions: [{ type: 'moveRelative', x: 0, y: 100, duration: 1500 }],
+            actions: [{ type: 'moveRelative', x: 0, y: DiffY, duration: 1500 }],
           },
           {
             target: CardFast,
-            actions: [{ type: 'moveRelative', x: 0, y: 100, duration: 1500 }],
-          },
-          {
-            target: CRM,
-            actions: [{ type: 'morph', duration: 1500 }],
-          },
-          {
-            target: ERP,
-            actions: [{ type: 'morph', duration: 1500 }],
+            actions: [{ type: 'moveRelative', x: 0, y: DiffY, duration: 1500 }],
           },
         ],
       },
@@ -60,41 +59,63 @@ const HomeScene: FC = () => {
             target: CardCluster,
             actions: [
               { type: 'unflip', duration: 0 },
-              { type: 'moveRelative', x: 0, y: -400, duration: 0 },
+              { type: 'move', x: 0, duration: 0 },
               { type: 'fade', opacity: 100, duration: 20 },
               { type: 'zIndex', value: 0 }, // Set specific layer
             ],
           },
         ],
       },
-      // {
-      //   delay: 2000, // After another 2 seconds
-      //   duration: 1500,
-      //   imageActions: [
-      //     {
-      //       target: I2,
-      //       actions: [
-      //         { type: 'flip', direction: 'front', duration: 1500 },
-      //         { type: 'moveRelative', x: 0, y: 300, duration: 1500 },
-      //         { type: 'fade', opacity: 0.3, duration: 1500 },
-      //       ],
-      //     },
-      //     {
-      //       target: I3,
-      //       actions: [{ type: 'moveRelative', x: 0, y: 100, duration: 0 }],
-      //     },
-      //     {
-      //       target: I1,
-      //       actions: [{ type: 'moveRelative', x: 0, y: 100, duration: 0 }],
-      //     },
-      //   ],
-      // },
+      // STEP 2
+      {
+        delay: 0, // Start immediately
+        duration: 1500, // This step takes 1500ms to complete
+        imageActions: [
+          {
+            target: CardOneInstance,
+            actions: [
+              { type: 'flip', direction: 'front', duration: 1500 },
+              { type: 'moveRelative', x: 0, y: DiffY, duration: 1500 },
+              { type: 'fade', opacity: 0, duration: 1500 },
+            ],
+          },
+          {
+            target: LineERPLeft,
+            actions: [{ type: 'draw' }],
+          },
+          {
+            target: CardCluster,
+            actions: [{ type: 'moveRelative', x: 0, y: DiffY, duration: 1500 }],
+          },
+          {
+            target: CardFast,
+            actions: [{ type: 'moveRelative', x: 0, y: DiffY, duration: 1500 }],
+          },
+        ],
+      },
+      {
+        delay: 100, // Wait 100ms after previous step completes
+        duration: 100, // This step takes 1500ms to complete
+        imageActions: [
+          {
+            target: CardOneInstance,
+            actions: [
+              { type: 'unflip', duration: 0 },
+              { type: 'moveRelative', x: 0, y: -(DiffY - 1) * NumCards, duration: 0 },
+              { type: 'fade', opacity: 100, duration: 20 },
+              { type: 'zIndex', value: 0 }, // Set specific layer
+            ],
+          },
+        ],
+      },
     ],
   }
 
   return (
     <CanvasScene scene={sceneDefinition}>
       {/* Example ImageFlip demonstrating parallel execution of flip, move, and fade */}
+
+      {/*FAST & FRIENDLY*/}
       <ImageFlip
         ref={CardFast}
         x={200}
@@ -107,10 +128,11 @@ const HomeScene: FC = () => {
         expansionScale={0.5}
       />
 
+      {/*ONE INSTANCE*/}
       <ImageFlip
         ref={CardOneInstance}
         x={200}
-        y={100}
+        y={DiffY}
         zIndex={1}
         scale={{ x: 1, y: 1 }}
         image="/canvas/cards/one-instance.card.png"
@@ -119,10 +141,11 @@ const HomeScene: FC = () => {
         expansionScale={0.5}
       />
 
+      {/*CLUSTER*/}
       <ImageFlip
         ref={CardCluster}
         x={200}
-        y={200}
+        y={2 * DiffY}
         zIndex={2}
         scale={{ x: 1, y: 1 }}
         image="/canvas/cards/cluster.card.png"
@@ -131,8 +154,44 @@ const HomeScene: FC = () => {
         expansionScale={0.5}
       />
 
+      <LineDraw
+        ref={LineCRMLeft}
+        x={50}
+        y={120}
+        endX={200}
+        endY={260}
+        stroke={2}
+        color={'#ff0000'}
+        duration={600}
+        deleteDelay={2000}
+      />
+
+      <LineDraw
+        ref={LineDBLeft}
+        x={50}
+        y={120}
+        endX={200}
+        endY={260}
+        stroke={2}
+        color={'#ff0000'}
+        duration={600}
+        deleteDelay={2000}
+      />
+
+      <LineDraw
+        ref={LineERPLeft}
+        x={50}
+        y={420}
+        endX={200}
+        endY={320}
+        stroke={2}
+        color={'#ff0000'}
+        duration={600}
+        deleteDelay={2000}
+      />
+
       <ImageMorph
-        ref={CRM}
+        ref={MorphCRMLeft}
         x={30}
         y={80}
         scale={0.8}
@@ -141,27 +200,15 @@ const HomeScene: FC = () => {
         duration={1000}
       />
 
-      <LineDraw
-        ref={LineCRM}
-        x={50}
-        y={120}
-        endX={200}
-        endY={260}
-        stroke={2}
-        color={'#ff0000'}
-        duration={600}
-        // deleteDelay={3000}
+      <ImageMorph
+        ref={MorphERPLeft}
+        x={30}
+        y={400}
+        scale={0.8}
+        image1={'/canvas/icons/erp.gray.png'}
+        image2={'/canvas/icons/erp.color.png'}
+        duration={1000}
       />
-
-      {/*<ImageMorph*/}
-      {/*  ref={ERP}*/}
-      {/*  x={30}*/}
-      {/*  y={200}*/}
-      {/*  scale={0.8}*/}
-      {/*  image1={'/canvas/icons/erp.gray.png'}*/}
-      {/*  image2={'/canvas/icons/erp.color.png'}*/}
-      {/*  duration={1000}*/}
-      {/*/>*/}
     </CanvasScene>
   )
 }
