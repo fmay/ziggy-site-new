2e9f5326bbc17dba58d411033dfb0022a36c8d54
Freddy May <freddymay@mac.com>
2025-12-02 11:48:27 +0000

    features page new approach

--- src/app/(product)/features/page.tsx
+++ src/app/(product)/features/page.tsx
@@ -1,18 +1,50 @@
 import PageLayout from '@/components/ui/page-wrappers/page-layout/PageLayout'
-import FlowFeatures from '@/app/(product)/flows/FlowFeatures'
-import HubspotBlockCollection from '@/components/content/hubspot/HubspotBlockCollection'
-import BlockCollections from '@/app/(product)/flows/BlockCollections'
 import JavascriptFeatures from '@/app/(product)/flows/JavascriptFeatures'
 import PageSection from '@/components/ui/page-wrappers/PageSection'
-import CTA from '@/components/ui/cta/CTA'
-import Intro from '@/components/ui/intro/Intro'
 import ScrollListCore from '@/app/(product)/features/ScrollList_Core'
 import ScrollListEdge from '@/app/(product)/features/ScrollList_Edge'
 import ScrollListExtra1 from '@/app/(product)/features/ScrollList_Extra1'
 import ScrollListExtra2 from '@/app/(product)/features/ScrollList_Extra2'
 import PerformanceFeatures from '@/app/(product)/features/PerformanceFeatures'
+import { ReactNode } from 'react'
+
+interface Section {
+  bgColor: string
+  children: ReactNode
+  id?: string
+  waveInvert?: boolean
+}
 
 export default function Features() {
+  const sections: Section[] = [
+    {
+      bgColor: 'pale-blue',
+      children: <ScrollListCore />,
+    },
+    {
+      bgColor: 'white',
+      children: <ScrollListEdge />,
+    },
+    {
+      id: 'javascript',
+      bgColor: 'pale-blue',
+      children: <JavascriptFeatures />,
+    },
+    {
+      id: 'extra1',
+      bgColor: 'white',
+      children: <ScrollListExtra1 />,
+    },
+    {
+      bgColor: 'bg-gray-200',
+      children: <ScrollListExtra2 />,
+    },
+    {
+      bgColor: 'bg-gray-50',
+      children: <PerformanceFeatures />,
+    },
+  ]
+
   return (
     <PageLayout
       title="Ziggy Features"
@@ -20,34 +52,17 @@ export default function Features() {
       description="Some of Ziggy's out-of-the-box features."
       image="/placeholder.svg">
 
-      {/*FLOWS, BLOCKS, DEBUGGER*/}
-      <PageSection bgColor="pale-blue" waveToColor="white">
-        <ScrollListCore />
-      </PageSection>
-
-      <PageSection bgColor="white" waveToColor="pale-blue">
-        <ScrollListEdge />
-      </PageSection>
-
-      {/*JAVASCRIPT*/}
-      <PageSection id="javascript" bgColor="pale-blue"  waveToColor="white">
-        <JavascriptFeatures />
-      </PageSection>
-
-      {/*EXTRA 1*/}
-      <PageSection id="extra1" bgColor="white" waveToColor="bg-gray-200">
-        <ScrollListExtra1 />
-      </PageSection>
-
-      {/*EXTRA 1*/}
-      <PageSection id="extra1" bgColor="bg-gray-200">
-        <ScrollListExtra2 />
-      </PageSection>
-
-      {/*CLUSTER*/}
-      <PageSection id="javascript" bgColor="bg-gray-50">
-        <PerformanceFeatures />
-      </PageSection>
+      {sections.map((section, index) => (
+        <PageSection
+          key={index}
+          id={section.id}
+          bgColor={section.bgColor}
+          waveToColor={sections[index + 1]?.bgColor}
+          waveInvert={section.waveInvert}
+        >
+          {section.children}
+        </PageSection>
+      ))}
 
     </PageLayout>
   )
